976278b1075c7f1e10ffa186906cb944fc3a1fef
Aleksei Smantser <lesha_smancer@mail.ru>
2021-02-12 10:20:00 +0300

    lesson 17

--- link_hunter/home/urls.py
+++ link_hunter/home/urls.py
@@ -3,5 +3,7 @@ from home import views as home
 
 
 urlpatterns = [
-    path("", home.home_page, name="home_page"),
+    path('', home.home_page, name='home_page'),
+    path('about/', home.about_page, name='about_page'),
+    path('contact/', home.contact_page, name='contact_page'),
 ]
--- link_hunter/link_hunter/settings.py
+++ link_hunter/link_hunter/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'link_hunter',
     'home',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'link_hunter' / 'static'

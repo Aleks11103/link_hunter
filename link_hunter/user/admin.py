30d73365a7f63284f7f9aa05f225e1a8e568b7b9
Aleksei Smantser <lesha_smancer@mail.ru>
2021-02-22 21:01:18 +0300

    22.02.2021

--- link_hunter/user/admin.py
+++ link_hunter/user/admin.py
@@ -5,22 +5,22 @@ from user.models import User
 class AdminUser(admin.ModelAdmin):
 
     list_display = ('username', 'last_login')
-    readonly_fields = ('username',)
+    # readonly_fields = ('username',)
     list_filter = (
         'username',
         'is_staff',
         'last_login',
     )
     search_fields = ('username', 'first_name',)
-    fieldsets = (
-        (None, {
-            'fields': (
-                'username',
-                'sex',
-                'last_login',
-            ),
-        }),
-    )
+    # fieldsets = (
+    #     (None, {
+    #         'fields': (
+    #             'username',
+    #             'sex',
+    #             'last_login',
+    #         ),
+    #     }),
+    # )
 
 
 admin.site.register(User, AdminUser)
--- link_hunter/user/models.py
+++ link_hunter/user/models.py
@@ -1,5 +1,21 @@
+import os
+import traceback
+from PIL import Image 
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models.signals import pre_delete, post_save
+from django.dispatch.dispatcher import receiver
+
+
+class DelQuarySet(models.query.QuerySet):
+    def delete(self):
+        for el in self:
+            el.delete()
+
+
+class DelManager(BaseUserManager):
+    pass
 
 
 class User(AbstractUser):
@@ -23,5 +39,31 @@ class User(AbstractUser):
     )
     avatar = models.ImageField(
         upload_to=path_upload,
+        null=True,
         verbose_name='Avatar',
     )
+    
+    def __str__(self):
+        return self.username
+
+    objects = DelManager()
+
+
+@receiver(pre_delete, sender=User)
+def user_delete(sender, instance, **kwargs):
+    instance.avatar.delete(False)
+
+
+@receiver(post_save, sender=User)
+def user_delete(sender, instance, **kwargs):
+    img_path = os.path.join(settings.MEDIA_ROOT, instance.avatar.path)
+    image = Image.open(img_path).convert('LA')
+    # image.show()
+
+
+    # def delete(self):
+    #     try:
+    #         os.unlink(os.path.abspath(self.avatar.path))
+    #     except Exception as error:
+    #         traceback.print_exc()
+    #     return super().delete()
